a4e3b3cbb5db2fead99e4049f775bc33d441dd3b
queenalfaro <queenalfaro@op.pl>
2026-07-08 22:43:28 +0500

    chore: add dev env

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 
 import asyncio 
+import json
 
 from youdo import Client
 
 
 async def main() -> None:
-
     client = await Client.create()
-    await client.start_listener()
+
+    result = client.get_config()
+    with open("config.json", "w", encoding="utf-8") as f:
+        json.dump(f, result, indent=4)
+
+    # await client.start_listener()
 
 
 if __name__ == "__main__":
